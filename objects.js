361fc154e399be6b67d7c37ff0f0b8086bdc1f42
exuberant-object-0311 <polly.c.reynolds@gmail.com>
2019-05-06 19:07:52 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,7 @@
-var playlist = {}
+var playlist = {
+  artistName:''
+  songTitle:''
+}
 
 function updatePlaylist(playlist, artistName, songTitle) {
 object.assign({artistName:'My Bloody Valentine',songTitle:'Slowdive'})
