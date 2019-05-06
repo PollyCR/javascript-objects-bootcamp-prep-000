bdf0270abe821d9b9414482466464f1e92e0d45a
exuberant-object-0311 <polly.c.reynolds@gmail.com>
2019-05-06 18:56:45 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,8 +1,8 @@
 var playlist = {
-  artistName:
-  songTitle:
+  artistName: '',
+  songTitle: ''
 }
 
-function updatePlaylist(playlist,artistName,songTitle) {
+function updatePlaylist(playlist, artistName, songTitle) {
   object.assign({},playlist, {})
 }
